569e2062c05a77ded67e9d8682f3c2bc3c09e9b1
026024 <120216635+Goncalo04Costa@users.noreply.github.com>
2023-11-02 23:31:25 +0000

    UCCI

--- UCCI/UCCI.cs
+++ UCCI/UCCI.cs
@@ -2,15 +2,50 @@
 using DIRETOR;
 using UTENTE;
 using FUNCIONARIO;
+using PESSOA;
 
 namespace UCCI
 {
     public class UCCI
     {
+        #region Atributos
         string nome;
         int capacidade;
         Utente[] utentes;
         Funcionario[] funcionarios;
         Diretor[] diretores;
+        #endregion
+        #region Metodos
+
+        #region Construtores
+        public UCCI()
+        {
+            nome = "";
+            capacidade = 0;
+        }
+        
+        public UCCI(string nome, int capacidade)
+        {
+            this.nome = nome;
+            this.capacidade = capacidade;
+    
+        }
+        #endregion
+
+        #region Propriedades
+        #endregion
+
+        #region Operadores
+        #endregion
+
+        #region Overrides
+        #endregion
+
+        #region Destruidores
+        #endregion
+
+        #endregion
+
+
     }
 }
